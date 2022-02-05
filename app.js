e2a404599ade444e9bfea82e6f537cd6cf959960
Adrian Del Valle <adrianchelo7@gmail.com>
2022-02-05 05:57:30 -0300

    Arreglos

--- app.js
+++ app.js
@@ -2,8 +2,11 @@ const express = require("express");
 const app = express();
 const PORT = process.env.PORT;
 
+app.set("view engine", "ejs");
+app.use(express.static("public"));
+
 app.get("/", (req, res) => {
-  res.end("HOLA");
+  res.render("home");
 });
 
 app.listen(PORT);
