6d43a3447c548d0a08225996fc3d8e861479e404
Adrian Del Valle <adrianchelo7@gmail.com>
2022-02-07 02:02:31 -0300

    updates

--- public/js/mainScrip.js
+++ public/js/mainScrip.js
@@ -1,19 +1,22 @@
 document.querySelector("#add-list").addEventListener("click", (e) => {
-  const lista = document
-    .querySelector("#input-list")
-    .value.trim()
-    .split(",")
-    .filter(Boolean);
+  const lista = document.querySelector("#input-list").value;
+
   const check = document.querySelector("#save-on-memory").checked;
   verificarContenido(lista, check);
 });
 
 function verificarContenido(lista, check) {
-  if (lista.length === 0) {
+  if (lista.trim() === "") {
     document.querySelector("#input-list").style.borderColor = "#f00";
   } else {
     document.querySelector("#input-list").style.borderColor = "#adb5bd";
-    for (const item, index of lista) {
+    const listaToArray = lista.trim().split(",").filter(Boolean);
+    console.log(listaToArray);
+
+    if (listaToArray.lenght === undefined && lista !== "") {
+      console.log("es un solo item");
+    } else {
+      /* for (const item, index of lista) {
       document.querySelector("#tbody-list").insertAdjacentHTML(
         "beforeend",
         `<tr>
@@ -21,6 +24,8 @@ function verificarContenido(lista, check) {
               <td>${item}</td>
             </tr>`
       );
+    } */
+      console.log("Hay mas de un item");
     }
   }
 }
