e5aca5af089d28c96f490bbba784e64a256ce845
Adrian Del Valle <adrianchelo7@gmail.com>
2022-02-07 01:49:41 -0300

    update

--- public/js/mainScrip.js
+++ public/js/mainScrip.js
@@ -1,11 +1,26 @@
 document.querySelector("#add-list").addEventListener("click", (e) => {
-  const lista = document.querySelector("#input-list").value;
+  const lista = document
+    .querySelector("#input-list")
+    .value.trim()
+    .split(",")
+    .filter(Boolean);
   const check = document.querySelector("#save-on-memory").checked;
   verificarContenido(lista, check);
 });
 
 function verificarContenido(lista, check) {
-  if (!lista) {
-    console.log("Esta vacio");
+  if (lista.length === 0) {
+    document.querySelector("#input-list").style.borderColor = "#f00";
+  } else {
+    document.querySelector("#input-list").style.borderColor = "#adb5bd";
+    for (const item, index of lista) {
+      document.querySelector("#tbody-list").insertAdjacentHTML(
+        "beforeend",
+        `<tr>
+              <th scope="row">${index + 1}</th>
+              <td>${item}</td>
+            </tr>`
+      );
+    }
   }
 }
