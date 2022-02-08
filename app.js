a7ce5f246ee074dc8e9ca385a2c5044e65257bbc
Adrian Del Valle <adrianchelo7@gmail.com>
2022-02-07 21:48:49 -0300

    Release 1.0
    Version final no ajustada pero funcional :D

--- app.js
+++ app.js
@@ -8,5 +8,12 @@ app.use(express.static("public"));
 app.get("/", (req, res) => {
   res.render("home");
 });
+app.get("/help", (req, res) => {
+  res.render("help");
+});
+
+app.get("/aboutme", (req, res) => {
+  res.render("aboutme");
+});
 
 app.listen(PORT);
